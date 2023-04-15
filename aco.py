73cc24aabdc9530aec1a0fe563a68b0eceb1a5f2
Lautaro Fernandez <lautaro-fernandez@hotmail.com>
2023-04-15 11:15:30 -0300

    Adding weight to distances and trail

--- aco.py
+++ aco.py
@@ -1,7 +1,9 @@
 import random
 import numpy as np
 
-REDUCTION_RATE = 0.2
+EVAPORATION_RATE = 0.5
+ALPHA:float = 1.0
+BETA:float = 1.0
 
 class Ant:
     
@@ -9,6 +11,7 @@ class Ant:
     ant_path:np.ndarray=np.empty(0, dtype=int)
     acu_distance:int
     next_point:int
+
     
     def __init__(self,start_location:int) -> None:
         self.location = start_location
@@ -17,32 +20,39 @@ class Ant:
         pass
     
     def ant_move(self, distances:np.ndarray, pheromone:np.ndarray) -> None:
-        #ToDo
+        
         options = np.arange(distances.size, dtype=int)
         probabilities = np.zeros(distances.size, dtype=float)
-                
+        distances_pondered:np.ndarray
+        pheromone_pondered:np.ndarray
+        epsilon = 1e-8 #To not divide by 0
+    
         for i in range (self.ant_path.size):
             options[self.ant_path[i]] = 0
+            distances[self.ant_path[i]] = 0
             pheromone[self.ant_path[i]] = 0
             pass
 
+        distances_pondered = distances + epsilon
+        pheromone_pondered = pheromone + epsilon
+        distances_pondered = np.power(distances, -BETA)
+        pheromone_pondered = np.power(pheromone, ALPHA)
+        distances_pondered[np.isinf(distances_pondered)] = 0
+        pheromone_pondered[np.isinf(pheromone_pondered)] = 0
+    
         for i in range (probabilities.size):
-            probabilities[i] = pheromone[i] * 1/(pheromone.sum())
+            if(distances_pondered[i] < np.Infinity and pheromone_pondered[i] < np.Infinity):
+                probabilities[i] = (pheromone_pondered[i]*distances_pondered[i])/(np.dot(pheromone_pondered,distances_pondered))
             pass
-
+        
         next_point = np.random.choice(options, p=probabilities)
         
         #consecuences of the move
         self.ant_path=np.append(self.ant_path, next_point)
         self.location = next_point
         self.acu_distance = self.acu_distance + distances[next_point]
-        
-        pass
-    def pheromone_update() -> int:
-        #ToDo
-        
-        pass
     
+        pass
     pass
 
 
@@ -51,6 +61,8 @@ def aco(size_graph,amount_ants) -> np.ndarray:
     distances:np.ndarray
     ants:np.ndarray = np.empty(amount_ants, dtype=Ant)
     pheromone:np.ndarray = np.ones((size_graph,size_graph), dtype=int)
+    cycles:int = 0
+    t:int = 0
     
     #Create a random graph of distances
     distances = np.zeros((size_graph,size_graph), dtype=int)
@@ -58,17 +70,19 @@ def aco(size_graph,amount_ants) -> np.ndarray:
     distances = distances + distances.T
     for i in range (amount_ants):
         np.put(ants,i,Ant(random.randint(0,size_graph-1)))
-        print("Hi I'm ant number: " + str(i) + " and my location is: " + str(ants[i].location))
         pass
-    while():
-
-        for i in range (amount_ants):
-            ants[i].ant_move(distances[ants[i].location], pheromone[ants[i].location])
+    
+    while(cycles < 2500):
+        while(t<size_graph-1):
+            for i in range (amount_ants):
+                ants[i].ant_move(distances[ants[i].location], pheromone[ants[i].location])
+                pass
+            t = t + 1
             pass
         for i in range (amount_ants):
-            ants[i].pheromone_update()
+            
+            pass
+        cycles = cycles + 1
     print (distances)
 
-    
-    
 aco(15,10)
